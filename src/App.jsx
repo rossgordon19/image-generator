dd00a18655a21a28760a2ef1d2d9b3f7ed6df5e8
Ross Gordon <rossagordonstl@gmail.com>
2023-01-10 22:01:24 -0500

    Added loading animation and updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,54 +3,62 @@ import { Configuration, OpenAIApi } from 'openai';
 
 function App() {
   const [prompt, setPrompt] = useState('');
+  const [result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
+
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
-  });  
+  });
 
   const openai = new OpenAIApi(configuration);
 
-  const [result, setResult] = useState("");
-
-const generateImage = async () => {
+  const generateImage = async () => {
+    setLoading(true);
     const res = await openai.createImage({
       prompt: prompt,
       n: 1,
-      size: "512x512",
+      size: '512x512',
     });
-
+    setLoading(false);
     setResult(res.data.data[0].url);
   };
 
   return (
     <div className="app-main">
-      <>
-        <h1 className="text-4xl p-4 font-bold">AI Image Generator</h1>
-
-        <textarea
-          className="p-2"
-          placeholder="Start typing to generate an image"
-          onChange={(e) => setPrompt(e.target.value)}
-          rows="10"
-          cols="40"
-        />
-        <p className="text-white pt-5 hover:underline">
-          Powered by{' '}
-          <a href="https://openai.com/api" target="_blank">
-            OpenAI API
-          </a>
-        </p>
-        <button
-          className="bg-[#F4D35E] p-2 font-bold mt-8"
-          onClick={generateImage}
-        >
-          Generate Image
-        </button>
-        {result.length > 0 ? (
-          <img className="result-image" src={result} alt="result" />
-        ) : (
-          <></>
-        )}
-      </>
+      {loading ? (
+        <>
+          <h2 className='text-white text-4xl text-center align-center'>Generating Image...</h2>
+          <div class="load"></div>
+        </>
+      ) : (
+        <>
+          <h1 className="text-white text-4xl p-4 font-bold">AI Image Generator</h1>
+          <textarea
+            className="p-2"
+            placeholder="Start typing to generate an image"
+            onChange={(e) => setPrompt(e.target.value)}
+            rows="10"
+            cols="40"
+          />
+          <p className="text-white pt-5 hover:underline">
+            Powered by{' '}
+            <a href="https://openai.com/api" target="_blank">
+              OpenAI API
+            </a>
+          </p>
+          <button
+            className="bg-[#F5CB5C] p-2 font-bold mt-8"
+            onClick={generateImage}
+          >
+            Generate Image
+          </button>
+          {result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
     </div>
   );
 }
